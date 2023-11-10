a12074d4c9a74e64a3fa7656f5371cc73fa0328c
AbhiGone <giriabhinaya@gmail.com>
2023-11-10 09:30:19 -0500

    prime checked

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("input a number");
+        int num = scan.nextInt();
+        boolean prime = true;
+
+        for(int i = 2; i <= num / 2; i++){
+            if(num % i == 0){
+                prime = false;
+                break;
+            }
+        }
+        if(prime){
+            System.out.println("your number is prime");
+        }else{
+            System.out.println("your number is not prime");
+        }
     }
 }
